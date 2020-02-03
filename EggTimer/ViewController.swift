e67bd3b56aba8217e395fb928c096e34630d6c7f
Zach Gwirtz <zachgw@gmail.com>
2020-02-02 20:35:25 -0800

    count down func

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -4,31 +4,32 @@ class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7] //dictionary
     
-    var counter = 60
-    
     var timer = Timer()
+    
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBOutlet weak var readyLabel: UILabel!
     @IBOutlet var progressBar: UIProgressView!
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        progressBar.progress = 1
-        
         timer.invalidate() // stops previous timer so only one runs at a time
-        
         let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
         
-        counter = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        readyLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
-    @objc func updateCounter() { // @objc refers to objective-c
-        if counter > 0 { // add 'almost done' logic
-            print("\(counter) seconds.")
-            counter -= 1
+    @objc func updateCounter() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
             readyLabel.text = "Eggs are cooking"
-        } else if counter == 0 {
+        } else {
             timer.invalidate()
             readyLabel.text = "Done!"
         }
